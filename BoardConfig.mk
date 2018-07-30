e1073d78249d0722109e4ac463ab65e0d89903e3
Ruthwar Ambeer <rambeer@codeaurora.org>
2018-07-30 11:19:00 +0530

    pine: Move AtFwd hal from device to framework manifest.
    AtFwd hal is implemented as an android service and
    it stars from system partition, that is, it's not
    a traditional vendor hal and hence it should be
    declared in the framework manifest and not in the
    device manifest.
    This commit introduces new device specific framework
    manifest (framework_manifest.xml) and moves AtFwd hal
    from device to framework manifest.
    
    Remove IConfig and IOemHook hals.
    
    Change-Id: Ibf4e5e4360a954ea6e4a4391969364e6ac35a139
    CRs-Fixed: 2287149

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ TARGET_NO_RPC := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
