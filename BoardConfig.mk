09e2a26e8797c08524062997ede98b7f1ba501be
xenon098 <38199318+xenon098@users.noreply.github.com>
2021-07-18 22:49:48 +0530

    pine: correct TARGET_ARCH_VARIANT

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@ BOARD_VENDOR := xiaomi
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
